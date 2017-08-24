8f86d4fab1a98cac6448aba47929f66d791a9b77
Dieison Santos <dieisonsantos@sou.faccat.br>
2017-08-23 21:20:34 -0300

    ideia de inicio de application console

--- Program_cadastro/Program_cadastro/Program.cs
+++ Program_cadastro/Program_cadastro/Program.cs
@@ -10,6 +10,43 @@ namespace Program_cadastro
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite uma opção abaixo:\n" +
+                "\n1. - INSERIR" +
+                "\n2. - ALTERAR" +
+                "\n3. - EXCLUIR" +
+                "\n4. - SAIR");
+
+            String s1 = null;
+
+            s1 = Console.ReadLine();
+
+            switch (s1)
+            {
+                case "1":
+                    List<String> nomes = new List<string>();
+
+                    nomes.Add("Dieison");  //0
+                    nomes.Add("Bruna");    //1
+                    nomes.Add("Anderson"); //2
+
+                    for (int i = 0; i < nomes.Count; i++)
+                    {
+                        Console.WriteLine(nomes[i]);
+                    }
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.WriteLine("ALTERAR");
+                    break;
+                case "3":
+                    Console.WriteLine("EXCLUIR");
+                    break;
+                case "4":
+                    Console.WriteLine("SAIR");
+                    break;
+
+            }
+            Console.ReadKey();
         }
     }
 }
